a320ec781ad0fa1aabc1221843c898c764b5913b
root <root@debian-obi2.obi2>
2016-05-11 03:49:17 +0200

    Commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app
+web: gunicorn -w 3 hello:app
--- hello.py
+++ hello.py
@@ -1,28 +1,12 @@
 from flask import Flask, render_template
-import os
-app = Flask(__name__)
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__, static_url_path='/static')
+#app.static_folder = os.path.dirname(os.path.abspath(__file__))
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', APP_ROOT)
-
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
